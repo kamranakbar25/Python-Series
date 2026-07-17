53e3d7a7232465e7ae5ed54d89706400f9cf157a
Kamran Akbar <findsorzo@gmail.com>
2026-07-17 18:26:18 +0530

    Loop Practice

---
+++ Lecture 3/main.py
@@ -0,0 +1,17 @@
+# Sum of digits of a number
+
+# Count the number of digits in a number
+
+# Reverse a number
+
+# Palindrome Number check
+
+# Prime number check
+
+# Armstrong number check
+
+# Square Pattern Printing
+
+# Triangle Pattern Printing
+
+# FizzBuzz Problem
